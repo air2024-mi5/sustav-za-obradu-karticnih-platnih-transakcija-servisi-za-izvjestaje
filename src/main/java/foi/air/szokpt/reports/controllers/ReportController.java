ba63dd6149250f765353b65bac0d01792ea8c6a7
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2024-12-10 17:57:17 +0100

    MI5-48 Dodani servis i kontroler
    Controller određuje akcije prilikom primitka upita. Servis odrađuje poslovnu logiku potrebnu za vraćanje odgovarajućeg odgovora.

---
+++ src/main/java/foi/air/szokpt/reports/controllers/ReportController.java
@@ -0,0 +1,33 @@
+package foi.air.szokpt.reports.controllers;
+
+import foi.air.szokpt.reports.dtos.responses.ApiResponse;
+import foi.air.szokpt.reports.dtos.responses.SuccessReportData;
+import foi.air.szokpt.reports.services.ReportService;
+import foi.air.szokpt.reports.util.ApiResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reports")
+public class ReportController {
+
+    private final ReportService reportService;
+
+    @Autowired
+    public ReportController(ReportService reportService){
+        this.reportService = reportService;
+    }
+
+    @GetMapping("/success")
+    public ResponseEntity<ApiResponse<SuccessReportData>> successReport(){
+        SuccessReportData data = reportService.getSuccessReport();
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ApiResponseUtil.successWithData("Succesful login", data)
+        );
+    }
+
+}
