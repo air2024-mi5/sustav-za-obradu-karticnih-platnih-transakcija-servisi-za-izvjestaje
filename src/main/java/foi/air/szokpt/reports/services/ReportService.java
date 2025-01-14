15821d9476bf1042d489e475ed3787bcc0be3508
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2025-01-14 16:09:37 +0100

    MI5-101 Dodana logika za dohvat i brojanje transakcija
    Klasa sadrži podatke o broju transakcija i broju transakcija prema kartičnom brand-u.
    
    Transakcije se dohvaćaju putem TransactionClient-a. A zatim se broje prema kartičnom brand-u.

--- src/main/java/foi/air/szokpt/reports/services/ReportService.java
+++ src/main/java/foi/air/szokpt/reports/services/ReportService.java
@@ -1,6 +1,7 @@
 package foi.air.szokpt.reports.services;
 
 import foi.air.szokpt.reports.clients.TransactionClient;
+import foi.air.szokpt.reports.dtos.responses.CardBrandsReportData;
 import foi.air.szokpt.reports.dtos.responses.SuccessReportData;
 import foi.air.szokpt.reports.entities.Transaction;
 import org.springframework.stereotype.Service;
@@ -42,6 +43,47 @@ public class ReportService {
                 successfulTransactions,
                 rejectedTransactions,
                 canceledTransactions
+        );
+    }
+
+    public CardBrandsReportData getCardBrandsReport(String authorizationHeader){
+        List<Transaction> transactions = client.getTransactions(authorizationHeader);
+
+        if(transactions == null)
+            return new CardBrandsReportData(0, 0, 0, 0, 0, 0, 0);
+
+        int visaCount = (int) transactions.stream()
+                .filter(t -> "Visa".equalsIgnoreCase(t.getCardBrand().toString()))
+                .count();
+
+        int dinersCount = (int) transactions.stream()
+                .filter(t -> "Diners".equalsIgnoreCase(t.getCardBrand().toString()))
+                .count();
+
+        int discoverCount = (int) transactions.stream()
+                .filter(t -> "Discover".equalsIgnoreCase(t.getCardBrand().toString()))
+                .count();
+
+        int mastercardCount = (int) transactions.stream()
+                .filter(t -> "Mastercard".equalsIgnoreCase(t.getCardBrand().toString()))
+                .count();
+
+        int maestroCount = (int) transactions.stream()
+                .filter(t -> "Maestro".equalsIgnoreCase(t.getCardBrand().toString()))
+                .count();
+
+        int amexCount = (int) transactions.stream()
+                .filter(t -> "Amex".equalsIgnoreCase(t.getCardBrand().toString()))
+                .count();
+
+        return new CardBrandsReportData(
+                transactions.size(),
+                visaCount,
+                dinersCount,
+                discoverCount,
+                mastercardCount,
+                maestroCount,
+                amexCount
         );
     }
 
